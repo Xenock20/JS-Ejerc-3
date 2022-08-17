ad0775e296790c1e3249c3dc13b66dd675962bfe
Xenock20 <101891933+Xenock20@users.noreply.github.com>
2022-08-17 18:48:47 -0300

    Add files via upload

---
+++ factorial-break.js
@@ -0,0 +1,12 @@
+let num = 1
+let i = 10;
+
+while(true){
+    if(i === 1){
+        break;
+    }
+    num *= i;
+        i--;
+}
+
+console.log(num);
---
+++ factorial-while.js
@@ -0,0 +1,9 @@
+let num = 1
+let i = 10;
+
+while(i > 1){
+    num *= i;
+    i--;
+}
+
+console.log(num);
